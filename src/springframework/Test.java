a7471c9584a748f321e17d774cd9a91719c963b2
cheruku <cheruku.vignan@gmail.com>
2018-02-20 13:37:56 -0500

    Constructor injection Collection

---
+++ src/springframework/Test.java
@@ -0,0 +1,17 @@
+package springframework;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+  
+public class Test {  
+public static void main(String[] args) {  
+
+	ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+    
+	Question obj = (Question) context.getBean("q");
+
+    obj.displayInfo();  
+      
+}  
+}  
